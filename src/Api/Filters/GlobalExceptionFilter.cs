4b22c4e0883247ec73a948c6c7cabae5e27252b3
Thiago Mata <thfmata@gmail.com>
2025-11-21 20:59:34 -0300

    feat: global exception

---
+++ src/Api/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Filters;
+
+public class GlobalExceptionFilter : IExceptionFilter
+{
+    private readonly ILogger<GlobalExceptionFilter> _logger;
+
+    public GlobalExceptionFilter(ILogger<GlobalExceptionFilter> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnException(ExceptionContext context)
+    {
+        var exception = context.Exception;
+
+        _logger.LogError(exception, "Ocorreu um erro: {Message}", exception.Message);
+
+        context.Result = exception switch
+        {
+            UnauthorizedAccessException => new ObjectResult(new { message = exception.Message })
+            {
+                StatusCode = StatusCodes.Status401Unauthorized
+            },
+            KeyNotFoundException => new ObjectResult(new { message = exception.Message })
+            {
+                StatusCode = StatusCodes.Status404NotFound
+            },
+            ArgumentException => new ObjectResult(new { message = exception.Message })
+            {
+                StatusCode = StatusCodes.Status400BadRequest
+            },
+            _ => new ObjectResult(new { message = "Ocorreu um erro interno no servidor." })
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            }
+        };
+
+        context.ExceptionHandled = true;
+    }
+}
